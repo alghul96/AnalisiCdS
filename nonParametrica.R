7fb86bb2544cfb38e0011b23d04359fdc577ec9a
alghul96 <gaetano.romano.96@gmail.com>
2017-05-03 14:24:53 +0200

    Aggiunti altri inutili grafici
    Dio ti prego fai laureare morig con 95 così non deve fare un colloquio
    per iscriversi alla magistrale

--- nonParametrica.R
+++ nonParametrica.R
@@ -1,24 +1,15 @@
 library(epicalc)
 library(survival)
 
-<<<<<<< HEAD
-=======
+
 ###########################
 #### OVERALL SURVIVAL #####
 
->>>>>>> origin/master
 
 # Creazione dell'oggetto survivial
 use(survival)
 
 
-<<<<<<< HEAD
-###########################
-#### OVERALL SURVIVAL #####
-
-
-=======
->>>>>>> origin/master
 msurv<- Surv(`OVERALL SURVIVAL (mesi)`, Morto == 1)
 
 mfit <- survfit((msurv)~1)
@@ -69,8 +60,87 @@ legend("topright", legend = c("Classe 0","Classe 1", "Classe 2"), col = c("red",
 
 #### Log Rank test
 
-<<<<<<< HEAD
 survdiff(msurv ~ survival$`LODDS CLASSE`, rho = 0) # rifiutiamo l'hp che le curve di sopravvivenza sono uguali
-=======
-survdiff(msurv ~ survival$`LODDS CLASSE`, rho = 0) # rifiutiamo l'hp che le curve di sopravvivenza sono uguali
->>>>>>> origin/master
+
+
+# Confronto tra le due curve
+
+par(mfrow = c(1,2))
+plot(mfit.LODDS, conf.int = FALSE, col = c("red", "green", "blue"), main = "Sopravvivenza per LODDS")
+plot(mfit.LNR, conf.int = FALSE, col = c("red", "green", "blue"), main = "Sopravvivenza per LNR")
+
+# Kaplan–Meier survival curves of the patients staged by log odds of positive lymph nodes (LODDS) and lymph node ratio.
+
+################################
+#### DISEASE-FREE SURVIVAL #####
+
+
+disurv<- Surv(`DISEASE- FREE SURVIVAL (mesi)`, Morto == 1)
+
+difit <- survfit((disurv)~1)
+options(survfit.print.mean = TRUE)
+my.fit = summary(difit)
+
+#### PLOT DELLA CURVA DELLA SOPRAVVIVENZA ####
+
+plot(difit, main = "Funzione di sopravvivenza Disease-Free", xlab = "Mesi dall'operazione")
+
+
+### STIMA DELLA CUMULATIVE HAZARD FUNCTION ####
+
+plot(difit, fun="cumhaz", main = "Funzione cumulativa del Rischio (-logS(t))") # funzione cumulativa di azardo 
+
+
+#### ANALISI DELLE COVARIATE: LNR ####
+
+#### Analisi della sopravvivenza
+
+difit.LNR = survfit(disurv ~ survival$`LNR CLASSE`)
+
+difit.LNR
+
+par(mfrow = c(1,2))
+plot(difit.LNR, conf.int = FALSE, col = c("red", "green", "blue"), main = "Disease-Free per LNR")
+plot(difit.LNR, conf.int = TRUE, col = c("red", "green", "blue"))
+legend("topright", legend = c("Classe 0","Classe 1", "Classe 2"), col = c("red", "green", "blue"), lty = c(1,1,1))
+
+#### Log Rank test
+
+survdiff(disurv ~ survival$`LNR CLASSE`, rho = 0) # rifiutiamo l'hp che le curve di sopravvivenza sono uguali
+
+
+
+#### ANALISI DELLE COVARIATE: LODDS ####
+
+#### Analisi della sopravvivenza
+
+difit.LODDS = survfit(disurv ~ survival$`LODDS CLASSE`)
+
+difit.LODDS
+
+par(mfrow = c(1,2))
+plot(difit.LODDS, conf.int = FALSE, col = c("red", "green", "blue"), main = "Disease-Free per LODDS")
+plot(difit.LODDS, conf.int = TRUE, col = c("red", "green", "blue"))
+legend("topright", legend = c("Classe 0","Classe 1", "Classe 2"), col = c("red", "green", "blue"), lty = c(1,1,1))
+
+#### Log Rank test
+
+survdiff(disurv ~ survival$`LODDS CLASSE`, rho = 0) # rifiutiamo l'hp che le curve di sopravvivenza sono uguali
+
+
+######################################################
+### CONFRONTO TRA DISEASE FREE E OVERALL SURVIVAL ####
+
+
+windows()
+par(mfrow = c(2,2))
+
+plot(mfit.LNR, conf.int = FALSE, col = c("red", "green", "blue"), main = "Sopravvivenza Totale per LNR")
+plot(difit.LNR, conf.int = FALSE, col = c("red", "green", "blue"), main = "Sopravvivenza Disease Free per LNR")
+legend("topright", legend = c("Classe 0","Classe 1", "Classe 2"), col = c("red", "green", "blue"), lty = c(1,1,1))
+
+
+plot(mfit.LODDS, conf.int = FALSE, col = c("red", "green", "blue"), main = "Sopravvivenza Totale per LODDS")
+plot(difit.LODDS, conf.int = FALSE, col = c("red", "green", "blue"), main = "Sopravvivenza Disease Free per LODDS")
+legend("topright", legend = c("Classe 0","Classe 1", "Classe 2"), col = c("red", "green", "blue"), lty = c(1,1,1))
+
