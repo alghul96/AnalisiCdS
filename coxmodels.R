a603ce0ace4248c27760f29b357bfc08a1ae109b
alghul96 <gaetano.romano.96@gmail.com>
2017-05-03 15:30:41 +0200

    aggkl

--- coxmodels.R
+++ coxmodels.R
@@ -2,4 +2,33 @@
 library(epicalc)
 library(survival)
 
+# recoding delle variabili
 
+modeldata = as.data.frame(rep(0, 202))
+
+modeldata[, "Asa"] = factor(survival$ASA)
+modeldata[,"nLinf12"] = factor(survival$`>12`)
+modeldata[, "stadio"] = factor(survival$STADIO)
+modeldata[, "ricN"] = factor(survival$Ric_N)
+modeldata[, "LNR"] = factor(survival$`LNR CLASSE`)
+modeldata[, "LODDS"] = factor(survival$`LODDS CLASSE`)
+modeldata = modeldata[,-1]
+str(modeldata)
+
+use(modeldata)
+
+#### Modello di Cox multivariato ####
+
+model0 = coxph(msurv ~ Asa + nLinf12 + stadio + ricN + LNR + LODDS)
+summary(model0)
+
+table(msurv, LNR)
+
+# Valutando la significatività
+
+summary(coxph(msurv ~ ricN + LNR))
+summary(coxph(msurv ~ LODDS + LNR))
+summary(coxph(msurv ~ LNR + LODDS))
+
+
+summary(co)
