4f2a460d822fdd3ac3ca18f8fae413eadccaa08b
alghul96 <gaetano.romano.96@gmail.com>
2017-05-05 10:38:03 +0200

    Aggiunti grafici per le ipotesi di cox

--- coxmodels.R
+++ coxmodels.R
@@ -2,6 +2,27 @@
 library(epicalc)
 library(survival)
 
+
+# Plot della log minus log, per vedere l'hp di rischi proporzionali
+
+autoplot(
+  mfit.LNR,
+  fun = "cloglog",
+  col = c("red", "green", "blue"),
+  #ylim = c(-2, 1),
+  conf.int = FALSE
+)
+
+autoplot(
+  mfit.LODDS,
+  fun = "cloglog",
+  col = c("red", "green", "blue"),
+  #ylim = c(-2, 1),
+  conf.int = FALSE
+)
+
+# Vediamo che l'ipotesi di rischi proporzionali sembra non essere rispettata
+
 # recoding delle variabili
 
 modeldata = as.data.frame(rep(0, 202))
@@ -16,6 +37,23 @@ modeldata = modeldata[,-1]
 str(modeldata)
 
 use(modeldata)
+
+
+#### MODELLI UNIVARIATI ####
+
+summary(coxph(msurv ~ LNR))
+
+# Come già visto, il coefficiente riferito alla classe 1 di LNR non è significativo.
+# Il coefficiente relativo alla classe 2, invece, risulta essere significativo. L'hazard ratio risulta essere, infatti, di 3.055
+# con valori che oscillano tra 1.89 e 4.9 con intervalli di confidenza del 95%
+
+summary(coxph(msurv ~ LODDS))
+
+# Situazione analoga per LODDS. La prima classe non risulta significativa pe run intervallo di confidenza del 95%.
+# La classe 2, invece, è significativa, con un HR di 3.8, con intervalli di confidenza al 95% che oscillano
+# da 2.257 a 6.396
+
+
 
 #### Modello di Cox multivariato ####
 
